41ae670c1fe91f05c6a6b42affa157133f8a1d52
Stepan <69470605+Rheamer@users.noreply.github.com>
2022-05-01 17:38:14 +0300

    Time: 2435 ms (41.73%), Space: 35.8 MB (25.90%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.py
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.py
@@ -4,7 +4,6 @@ class Solution:
         for i in range(len(pT)):
             nw.append([gT[i], pT[i]])
         nw.sort(reverse=True)
-        print(nw)
         for i in range(1, len(nw)):
             newpT= nw[i-1][1] + nw[i][1]
             nw[i][0]= min(max(nw[i][0], nw[i-1][0]-nw[i][1]), max(nw[i-1][0],nw[i][0]-nw[i-1][1]))
