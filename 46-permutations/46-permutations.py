abbae1255b4d7d839567e775b5bd3a9708b9f0c1
Stepan <69470605+Rheamer@users.noreply.github.com>
2022-06-17 13:19:38 +0300

    Time: 52 ms (64.54%), Space: 14.2 MB (21.59%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,16 +1,24 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res = []
         
-        def rec(nums, bitm, arr):
-            perms = False
-            for i in range(len(nums)):
-                if not bitm & 1<<i:
-                    perms = True
-                    newbitm = bitm | (1 << i)
-                    rec(nums, newbitm, arr + [nums[i]])             
-            if perms == False:
-                res.append(arr)
-            return
-        rec(nums,0,[])
-        return res
+        def backtrack(first):
+            if first == n:
+                output.append(nums[:])
+                
+            for i in range(first, n):
+                nums[first], nums[i] = nums[i], nums[first]
+                
+                backtrack(first+1)
+                
+                nums[first], nums[i] = nums[i], nums[first]
+            
+            
+            
+        
+        n = len(nums)
+        
+        output = []
+        
+        backtrack(0)
+        
+        return output
