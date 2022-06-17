ded99820036760848130acbba9a2eda0565a1d3b
Stepan <69470605+Rheamer@users.noreply.github.com>
2022-06-17 20:58:04 +0300

    Time: 875 ms (53.00%), Space: 33.3 MB (10.19%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -0,0 +1,21 @@
+class Solution:
+    def partition(self, ss: str) -> List[List[str]]:
+        sdict = {}
+        def rec(s):
+            if s in sdict:
+                return sdict[s]
+            resarr = []
+            if len(s) == 1:
+                return [[s]]
+            if len(s) == 0:
+                return []
+            for i in range(1,len(s)+1):
+                if s[0:i] == s[i-1::-1]:
+                    for r in rec(s[i:len(s)]):
+                        resarr.append([s[0:i]]+r)
+                    if len(s[i:len(s)]) == 0:
+                        resarr.append([s[0:i]])
+            sdict[s] = resarr
+            return resarr
+        
+        return rec(ss)
