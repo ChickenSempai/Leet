b0e0b4453c9c1d430739f2d12afad0bac038d0b4
Stepan <69470605+Rheamer@users.noreply.github.com>
2022-06-25 15:28:31 +0300

    Time: 14 ms (56.23%), Space: 10.3 MB (78.93%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        std::unordered_set<int> visited;
+        std::bitset<1000> visited;
         std::queue<int> q;
         q.push(0);
         while(!q.empty()){
             auto curr = q.front();
-            if (visited.find(curr) == visited.end()){
-                visited.insert(curr);
+            if (!visited[curr]){
+                visited.set(curr);
                 for(auto& key: rooms[curr])
                     q.push(key);
             }
             q.pop();
         }
-        if (visited.size() == rooms.size())
-            return true;
-        return false;
+        return visited.count() == rooms.size();
     }
 };
