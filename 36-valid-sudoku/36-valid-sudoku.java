ee460b2292a68c63d0ea6586126b5b86cd6cf38a
Stepan <69470605+Rheamer@users.noreply.github.com>
2022-08-12 15:40:50 +0300

    Time: 105 ms (5.29%), Space: 64.3 MB (5.27%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        ArrayList<HashSet<Character>> vertical = new ArrayList<HashSet<Character>>(Collections.nCopies(9, new HashSet<Character>()));
+        ArrayList<HashSet<Character>> horizontal = new ArrayList<HashSet<Character>>(Collections.nCopies(9, new HashSet<Character>()));
+        ArrayList<HashSet<Character>> cubes = new ArrayList<HashSet<Character>>(Collections.nCopies(9, new HashSet<Character>()));
+        for(int i=0; i < 9; i++){
+            vertical.set(i, new HashSet());
+            horizontal.set(i, new HashSet());
+            cubes.set(i, new HashSet());
+        }
+        System.out.println(vertical.size());
+        for(int i=0; i < 9; i++){
+            for(int j=0; j<9; j++){
+                if (board[i][j]!='.') {
+                    if (vertical.get(i).contains(board[i][j])) {
+                        return false;
+                    }
+                    if (horizontal.get(j).contains(board[i][j])) {
+                        return false;
+                    }
+                    if (cubes.get((int) Math.floor(i / 3) * 3 + (int) Math.floor(j / 3)).contains(board[i][j])) {
+                        return false;
+                    }
+                    vertical.get(i).add(board[i][j]);
+                    horizontal.get(j).add(board[i][j]);
+                    cubes.get((int) Math.floor(i / 3) * 3 + (int) Math.floor(j / 3)).add(board[i][j]);
+                    System.out.println(vertical.toString());
+                    System.out.println(horizontal.toString());
+                    System.out.println(cubes.toString());
+                    System.out.println(' ');
+                }
+            }
+        }
+        return true;
+    }
+}
